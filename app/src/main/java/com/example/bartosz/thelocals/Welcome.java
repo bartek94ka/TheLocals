ee23bbf955ab32c7c2a863d9372965b77b968cc3
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2018-07-23 20:53:31 +0200

    Creating welcome page + app logo

--- app/src/main/java/com/example/bartosz/thelocals/Welcome.java
+++ app/src/main/java/com/example/bartosz/thelocals/Welcome.java
@@ -2,6 +2,7 @@ package com.example.bartosz.thelocals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 
 public class Welcome extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class Welcome extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_welcome);
     }
 }
