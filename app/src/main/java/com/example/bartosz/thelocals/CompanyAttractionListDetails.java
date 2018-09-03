cff79b71658bc4cbd7b0dfc39adb6f3f6340d8b9
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2018-09-03 21:15:16 +0200

    Changing names for fragment

--- app/src/main/java/com/example/bartosz/thelocals/CompanyAttractionListDetails.java
+++ app/src/main/java/com/example/bartosz/thelocals/CompanyAttractionListDetails.java
@@ -12,14 +12,14 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AttractionListDetails.OnFragmentInteractionListener} interface
+ * {@link CompanyAttractionListDetails.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
-public class AttractionListDetails extends Fragment {
+public class CompanyAttractionListDetails extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public AttractionListDetails() {
+    public CompanyAttractionListDetails() {
         // Required empty public constructor
     }
 
@@ -28,7 +28,7 @@ public class AttractionListDetails extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_attraction_list_details, container, false);
+        return inflater.inflate(R.layout.fragment_company_attraction_list_details, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
